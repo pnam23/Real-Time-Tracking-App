6a722791933f5c0bf62261b1054d66bc2d281d4a
Lê Nguyễn Phương Nam <97205910+pnam23@users.noreply.github.com>
2024-09-22 21:43:21 +0700

    done

---
+++ public/js/script.js
@@ -0,0 +1,50 @@
+const socket = io();
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords; // Corrected typo
+            socket.emit("send-location", { latitude, longitude });
+        },
+        (error) => {
+            console.error("Geolocation error:", error);
+        },
+        {
+            enableHighAccuracy: true,  // Use high-accuracy location
+            timeout: 5000,             // Timeout after 5 seconds
+            maximumAge: 0              // No cached position
+        }
+    );
+} else {
+    console.error("Geolocation is not supported by this browser.");
+}
+
+
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution: "OpenStreetMap"
+}).addTo(map)
+
+
+const markers = {};
+socket.on("receive-location", (data)=>{
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude], 16);
+
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    }
+    else{
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+
+socket.on("disconnect", (id) =>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
