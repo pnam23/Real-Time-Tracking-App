b22e27ae3a101d6f8615a7198bba5dc7379f4b2b
Lê Nguyễn Phương Nam <97205910+pnam23@users.noreply.github.com>
2024-09-24 20:31:27 +0700

    ready for deployment

--- public/js/script.js
+++ public/js/script.js
@@ -24,7 +24,7 @@ if (navigator.geolocation) {
 const map = L.map("map").setView([0, 0], 10);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution: "OpenStreetMap"
+    attribution: "Le Nguyen Phuong Nam"
 }).addTo(map)
 
 
